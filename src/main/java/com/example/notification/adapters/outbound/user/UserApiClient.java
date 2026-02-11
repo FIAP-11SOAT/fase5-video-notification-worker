43a56af3edccce521e8fdaa75453567794395cd2
Amanda <mandacosta94@gmail.com>
2026-02-10 22:54:33 -0300

    deploy

--- src/main/java/com/example/notification/adapters/outbound/user/UserApiClient.java
+++ src/main/java/com/example/notification/adapters/outbound/user/UserApiClient.java
@@ -1,18 +1,17 @@
 package com.example.notification.adapters.outbound.user;
 
 import com.example.notification.adapters.outbound.dto.UserResponse;
-import feign.Headers;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(
         name = "userClient",
-        url = "${auth.service.url}"
+        url = "${auth.service.url}",
+        configuration = UserApiClientConfig.class
 )
 public interface UserApiClient {
 
     @GetMapping("/auth/user-by-id/{userId}")
-    @Headers("X-MS-Token: dXMtZWFzdC0xX0xPamx4NWIzSDo3aHMxaG80aXYybTYxYmptYmNmMzRtZnFs")
     UserResponse getUserById(@PathVariable String userId);
 }
