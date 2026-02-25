a6e8e5cc8564c23f9b577ad4af61a731022a8e7a
Amanda <mandacosta94@gmail.com>
2026-02-24 21:34:17 -0300

    configurando deploy

--- deploy/terraform/variables.tf
+++ deploy/terraform/variables.tf
@@ -1,45 +1,38 @@
-  variable "aws_region" {
-    description = "AWS Region"
-    type        = string
-    default     = "us-east-1"
-  }
+data "aws_region" "current" {}
+data "aws_caller_identity" "current" {}
+data "aws_ecr_authorization_token" "ecr_auth" {}
 
-  variable "cluster_name" {
-    description = "Nome do cluster EKS"
-    type        = string
-  }
+# Fetching master secrets
+data "aws_secretsmanager_secret" "master_secrets" {
+  name = "terraform-master-credentials"
+}
+data "aws_secretsmanager_secret_version" "master_secrets" {
+  secret_id = data.aws_secretsmanager_secret.master_secrets.id
+}
 
-  variable "namespace" {
-    description = "Namespace do Kubernetes"
-    type        = string
-    default     = "fase5-video-notification-worker"
-  }
+# Fetching infrastructure-specific secrets
+data "aws_secretsmanager_secret" "infra_secrets" {
+  name = "fase5-video-processing-infra-secrets"
+}
+data "aws_secretsmanager_secret_version" "infra_secrets" {
+  secret_id = data.aws_secretsmanager_secret.infra_secrets.id
+}
 
-  variable "service_account_name" {
-    description = "Nome do Service Account"
-    type        = string
-    default     = "video-notification-worker-sa"
-  }
+# Decoding secrets and processing ECR endpoint
+locals {
+  aws_infra_secrets           = jsondecode(data.aws_secretsmanager_secret_version.infra_secrets.secret_string)
+  aws_master_secrets          = jsondecode(data.aws_secretsmanager_secret_version.master_secrets.secret_string)
+  aws_ecr_auth_proxy_endpoint = replace(data.aws_ecr_authorization_token.ecr_auth.proxy_endpoint, "https://", "")
+}
 
-  variable "sqs_queue_name" {
-    description = "Nome da fila SQS"
-    type        = string
-    default     = "fase5-video-notification-queue"
-  }
+variable "aws_region" {
+  description = "The AWS region to deploy resources in"
+  type        = string
+  default     = "us-east-1"
+}
 
-  variable "s3_bucket_name" {
-    description = "Nome do bucket S3"
-    type        = string
-  }
-
-  variable "dynamodb_table_name" {
-    description = "Nome da tabela DynamoDB"
-    type        = string
-    default     = "fase5-video-processing"
-  }
-
-  variable "sns_topic_arn" {
-    description = "ARN do topico SNS para notificacoes (opcional)"
-    type        = string
-    default     = ""
-  }
+variable "project_name" {
+  description = "The name of the project"
+  type        = string
+  default     = "fase5-video-notification-worker"
+}
